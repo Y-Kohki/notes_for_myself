0ebb9fb79c671eb6218f9e3d0770dd569798b44c
Y-Kohki <kohki000608@yahoo.co.jp>
2022-02-02 21:34:19 +0900

    api 認証

--- database/migrations/2021_12_20_205327_create_customs_table.php
+++ database/migrations/2021_12_20_205327_create_customs_table.php
@@ -17,6 +17,15 @@ class CreateCustomsTable extends Migration
             $table->increments('id');
             $table->string('custom');
         });
+        DB::table('customss')->insert([
+        ['custom' => 'extra_whip'],
+        ['custom' => 'light_whip'],
+        ['custom' => 'no_whip'],
+        ['custom' => 'extra_sourse'],
+        ['custom' => 'light_sourse'],
+        ['custom' => 'extra_syrup'],
+        ['custom' => 'light_syrup'],
+    ]);
     }
 
     /**
